35b3d464448915b97432afb875c977ce3430d260
Cristobal Selame <cristobal.selame@sfcm.edu>
2023-07-27 17:50:09 -0400

    hola

--- 01-Ruby/01-Programming-basics/Optional-02-Ip-Address-Converter/lib/ip.rb
+++ 01-Ruby/01-Programming-basics/Optional-02-Ip-Address-Converter/lib/ip.rb
@@ -1,7 +1,9 @@
 def ip_to_num(ip_address)
-  # TODdsaO: return the number version of the `ip_address` string
+  # TODO: return the number version of the `ip_address` string
 end
 
 def num_to_ip(number)
   # TODO: return the string IP address from the `number`
 end
+
+ad
