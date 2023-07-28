1cf183a117953589a791fad35ed4db8caddd8262
Cristobal Selame <cristobal.selame@sfcm.edu>
2023-07-27 23:47:35 -0400

    stupid coaching

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,7 +1,14 @@
 def coach_answer(your_message)
   # TODO: return coach answer to your_message
+    if your_message[-1] == "?"
+      puts "Silly question, get dressed and go to work!"
+    else
+      puts "I don't care, get dressed and go to work!"
+  end
 end
 
 def coach_answer_enhanced(your_message)
-  # TODO: return coach answer to your_message, with additional custom rules of yours!
+
 end
+
+coach_answer("")
--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,2 +1,9 @@
 require_relative "coach_answer"
 # TODO: Implement the program that makes you discuss with your coach from the terminal.
+def coach_answer(your_message)
+  # TODO: return coach answer to your_message
+  while your_message != "I am going to work right now!"
+    puts "Hi I am your coach"
+    your_message = gets.chomp
+  end
+end
